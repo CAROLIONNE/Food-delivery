de8182ac221665027b0ceafb78a07fc41ab2ed21
caroline soc <soc.caroline1991@gmail.com>
2022-09-28 16:53:31 +0200

    init projet

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import logo from '../../assets/images/res-logo.png'
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <div className="nav_wrapper">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h5>Tasty Treat</h5>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
