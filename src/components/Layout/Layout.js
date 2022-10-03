594184f45ee4c45efd60103489eca25ced5711f8
caroline soc <soc.caroline1991@gmail.com>
2022-10-03 18:31:15 +0200

    display cart

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,17 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routes from "../../routes/Router";
+import Carts from "../../UI/category/cart/Carts";
+import { useSelector } from "react-redux";
 
 const Layout = () => {
+  const showCart = useSelector((state) => state.cartUi.cartIsVisible);
+
   return (
     <div>
       <Header />
+      {showCart && <Carts />}
+
       <div>
         <Routes />
       </div>
