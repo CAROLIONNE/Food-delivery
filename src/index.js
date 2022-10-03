c9375fb5e346f70d07189cdf5adab6ae3209e16d
caroline soc <soc.caroline1991@gmail.com>
2022-10-03 08:55:34 +0200

    hashRouteur/ github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
