bc5c8aa784a756dd0d700f3cdef1bd9b8d1f48fb
caroline soc <soc.caroline1991@gmail.com>
2022-10-06 11:14:44 +0200

    sort & filter products

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -11,25 +11,52 @@ import "../styles/pagination.css";
 const AllFoods = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [pageNumber, setPageNumber] = useState(0);
-  const searchedProduct = products.filter((item) => {
-    if (searchTerm.value === "") return item;
-    if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
-      return item;
-  });
+  const [sortCategory, setSortCategory] = useState("Default");
+  const [allProducts, setAllProducts] = useState(products);
+
+  // Pagination
   const productPerPage = 8;
   const visitedPage = pageNumber * productPerPage;
-  const displayPage = searchedProduct.slice(
+  const displayPage = allProducts.slice(
     visitedPage,
     visitedPage + productPerPage
   );
-  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+  const pageCount = Math.ceil(allProducts.length / productPerPage);
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
+
+  useEffect(() => {
+    // Searched Product
+    function searchedProduct() {
+      let tab = products.filter((item) => {
+        if (searchTerm === "") return true;
+        if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+          return true;
+        return false;
+      });
+      return tab;
+    }
+    // Sort products
+    let tab = searchedProduct();
+    if (sortCategory === "ascending") {
+      tab = tab.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    if (sortCategory === "descending") {
+      tab = tab.sort((a, b) => b.title.localeCompare(a.title));
+    }
+    if (sortCategory === "high-price") {
+      tab = tab.sort((a, b) => b.price - a.price);
+    }
+    if (sortCategory === "low-price") {
+      tab = tab.sort((a, b) => a.price - b.price);
+    }
+    setAllProducts(tab);
+  }, [sortCategory, searchTerm]);
+
   return (
     <Helmet title="All-Foods">
       <CommonSection title="All Foods" />
-
       <section>
         <Container>
           <Row>
@@ -48,8 +75,11 @@ const AllFoods = () => {
             </Col>
             <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
               <div className="sorting__widget text-end">
-                <select className="w-50">
-                  <option>Default</option>
+                <select
+                  className="w-50"
+                  onChange={(e) => setSortCategory(e.target.value)}
+                >
+                  <option value="default">Default</option>
                   <option value="ascending">Alphabetically, A-Z</option>
                   <option value="descending">Alphabetically, Z-A</option>
                   <option value="high-price">High Price</option>
